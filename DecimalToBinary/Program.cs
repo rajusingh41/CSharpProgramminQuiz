4e329bef332dda7e12c6ab0cd312aaaea00ebd29
rajusingh41 <raju.singh41@gmail.com>
2019-05-28 13:49:35 +0530

    Modify

---
+++ DecimalToBinary/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecimalToBinary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userNumber, i;
+            int[] arr = new int[10];
+
+            Console.Write("Enter number which you want to convert to binary:");
+
+            userNumber = Convert.ToInt32(Console.ReadLine());
+             for(i=0;userNumber>0;i++)
+            {
+                arr[i] = userNumber % 2;
+                userNumber /= 2;
+            }
+            // wirte binary 
+
+            Console.Write("Binary Number is :");
+
+
+            for(i=i-1;i>= 0;i--)
+            {
+                Console.Write(arr[i]);
+            }
+            Console.Read();
+        }
+    }
+}
