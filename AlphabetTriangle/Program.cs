0f6f16ddca890a7bc26e06facf2635d7594770b9
Raju Singh <raju.singh41@gmail.com>
2019-06-05 23:48:43 +0530

    alphabet traingle

--- AlphabetTriangle/Program.cs
+++ AlphabetTriangle/Program.cs
@@ -10,6 +10,37 @@ namespace AlphabetTriangle
     {
         static void Main(string[] args)
         {
+            int numberOfRows;
+            Console.Write("Enter number that you want to generate rows :");
+            numberOfRows = Convert.ToInt32(Console.ReadLine());
+
+            char alphabet = 'A';
+
+            for (int i=0;i<=numberOfRows;i++)
+            {
+
+                for(int j=numberOfRows;j>=i;j--)
+                {
+                    Console.Write(" ");
+                }
+                for(int k=1;k<=i;k++)
+                {
+                    Console.Write(alphabet++);
+                    
+                }
+                alphabet--;
+                for (int l = 1; l < i;l++)
+                {
+                    Console.Write(--alphabet);
+
+                }
+                Console.Write("\n");
+                alphabet = 'A';
+            }
+
+
+            Console.Read();
+
         }
     }
 }
